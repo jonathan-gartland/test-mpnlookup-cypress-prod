ddf2326b1eebb9d388db5c954647220cc539bf17
jonathan-gartland <jonathan-gartland@gmail.com>
2023-12-05 12:41:42 -0500

    add my url to test

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require("cypress");
+const dotenv  = require('dotenv');
+dotenv.config()
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: `${process.env.PROD_URL}`,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
